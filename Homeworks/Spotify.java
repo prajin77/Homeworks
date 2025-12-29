3e569c1c7feb41e9a0ad49ccda95f8e1f409795f
prajin77 <vprajin77@gmail.com>
2025-12-29 12:57:14 +0530

    Add files via upload

---
+++ Homeworks/Spotify.java
@@ -0,0 +1,264 @@
+import java.util.Scanner;
+class Spotify
+{
+	public static void main(String[] args)
+	{
+ 		System.out.println("SPOTIFY WELCOMES YOU");
+		System.out.println("1. A.R. RAHMAN.");
+		System.out.println("2. ANIRUDH ");
+		System.out.println("3. HARISH JEYARAJ  "); 
+		System.out.println("4. ILLAIYARAJA ");	
+		System.out.println("5. U1");
+		Scanner sc=new Scanner(System.in);
+		System.out.print("SELECT YOUR OPTION, THE OPTIONS ARE:");
+		int option=sc.nextInt();
+		if(option==1)
+		{
+			System.out.println("Welcome to A.R HIts");
+			System.out.println("1. Melody");
+			System.out.println("2. Kuthu");
+			System.out.println("3. Motivational Song");
+			System.out.println("4. Sad ");
+			System.out.print("Enter your choice:");
+			int choice=sc.nextInt();
+			if(choice==1)
+			{
+				System.out.println("1.Welcome to AR Melody");
+				System.out.println("*Netru illadha Matram ");
+				System.out.println("*Enna Solla Pogirai ");
+				System.out.println("*Ennavale Adi Ennavale ");
+				System.out.println("*Malargale Malargale ");
+			}	
+			else if(choice==2)
+			{
+					
+				System.out.println("Welcome to Ar Kuthu song ");
+				System.out.println(".Thaiya Thaiya ");
+				System.out.println(".Mukkala Mukkabala ");	
+				System.out.println(".Urvashi Urvashi  ");
+				System.out.println(".Aathichudi ");
+					
+			}
+			else if(choice==3)
+			{
+				System.out.println("Welcome to Ar Motivational  song");
+				System.out.println("• Vande Mataram ");
+				System.out.println("• Oruvan Oruvan mudhalli");
+				System.out.println("• Shakthi kodu");
+				System.out.println("• Kappal Yeari Poyachu ");
+			}
+			else if(choice==4)
+			{
+				
+				System.out.println("Welcome to ar sad songs");
+				System.out.println("• Poongatrile ");
+				System.out.println("• Marudaani ");
+				System.out.println("• Minnalea En vazhvin ");
+				System.out.println("• Rasathi");					
+			}
+			else
+			{
+				System.out.println("invalid song");
+			}	
+			
+		}
+		else if(option==2)
+		{
+			System.out.println("Welcome to ANIRUDH HIts");
+			System.out.println("1. Melody");
+			System.out.println("2. Kuthu");
+			System.out.println("3. Motivational Song");
+			System.out.println("4. Sad ");
+			System.out.print("Enter your choice:");
+			int choice=sc.nextInt();
+			if(choice==1)
+			{
+				System.out.println("Welcome to ANIRUDH Melody");
+				System.out.println("• Neeyum Nanum Anbe ");
+				System.out.println("• Velicha Poove ");
+				System.out.println("• Po Indru Neeyaga ");
+				System.out.println("• Nee partha vizhigal ");
+			}	
+			else if(choice==2)
+			{
+					
+				System.out.println("Welcome to ANIRUDH Kuthu song ");
+				System.out.println("• Vaathi Raid ");
+				System.out.println("• Naa Ready ");	
+				System.out.println("• Badass  ");
+				System.out.println("• Vandha Edam  ");
+					
+			}
+			else if(choice==3)
+			{
+				System.out.println("Welcome to ANIRUDH Motivational  song");
+				System.out.println("•Velai Illa pattathari    ");
+				System.out.println("•Boomi Enna Suthudhe  ");
+				System.out.println("•Ethir Neechal  ");
+				System.out.println("•• Surviva   ");
+			}
+			else if(choice==4)
+			{
+				
+				System.out.println("Welcome to ANIRUDH sad songs");
+				System.out.println("• Porkanda Singam ");
+				System.out.println("• Kanave Kanave  ");
+				System.out.println("• Kannaana Kanne   ");
+				System.out.println("• Jodi Nilave  ");				
+			}
+			else
+			{
+				System.out.println("invalid song");
+			}
+			
+		}
+		else if(option==3)
+		{
+			System.out.println("Welcome to HARISH JEYARAJ HIts");
+			System.out.println("1. Melody");
+			System.out.println("2. Kuthu");
+			System.out.println("3. Sad ");
+			System.out.print("Enter your choice:");
+			int choice=sc.nextInt();
+			if(choice==1)
+			{
+				System.out.println("Welcome to HARISH JEYARAJ  Melody");
+				System.out.println("•Unnale ");
+				System.out.println("•Annul Maele ");
+				System.out.println("•Yedho Ondru");
+				System.out.println("•Paartha Mudhal");
+			}	
+			else if(choice==2)
+			{
+					
+				System.out.println("Welcome to HARISH JEYARAJ  Kuthu song ");
+				System.out.println("•Tirunelveli Halwa Da ");
+				System.out.println("•Aradi katre ");	
+				System.out.println("•Soda Bottle  ");
+				System.out.println("• Aruva Meesai  ");
+					
+			}
+			
+			else if(choice==3)
+			{
+				
+				System.out.println("Welcome to HARISH JEYARAJ  sad songs");
+				System.out.println("• Venmathiye  ");
+				System.out.println("•Oh maname   ");
+				System.out.println("• Yamma Yamma    ");
+				System.out.println("•Othayile  ");					
+			}
+			else
+			{
+				System.out.println("invalid song");
+			}
+			
+		}
+		else if(option==4)
+		{
+			System.out.println("Welcome to ILLAIYARAJA  HIts");
+			System.out.println("1. Melody");
+			System.out.println("2. Kuthu");
+			System.out.println("3. Sad ");
+			System.out.print("Enter your choice:");
+			int choice=sc.nextInt();
+			if(choice==1)
+			{
+				System.out.println("Welcome to ILLAIYARAJA   Melody");
+				System.out.println("•Oru kili uruguthu  ");
+				System.out.println("•Kuzhaloodhum kannanukku");
+				System.out.println("•Ennai thottu");
+				System.out.println("•Raja Raja cholan");
+			}	
+			else if(choice==2)
+			{
+					
+				System.out.println("Welcome to ILLAIYARAJA   Kuthu song ");
+				System.out.println("•Ooruvittu ooruvanthu  ");
+				System.out.println("•Nooru varusham  ");	
+				System.out.println("• Aasai Nooruvagai ");
+				System.out.println("•Aasai Athigam vachu  ");
+					
+			}
+			
+			else if(choice==3)
+			{
+				
+				System.out.println("Welcome to ILLAIYARAJA   sad songs");
+				System.out.println("•Kanne Kalaimane   ");
+				System.out.println("•Thene thenpaandi Neeye   ");
+				System.out.println("•Paadi Parantha Killi    ");
+				System.out.println("•Nilave Vaa ");					
+			}
+			else
+			{
+				System.out.println("invalid song");
+			}
+		}
+		else if(option==5)
+		{
+			System.out.println("Welcome to ILLAIYARAJA  HIts");
+			System.out.println("1. Melody");
+			System.out.println("2. Kuthu");
+			System.out.println("3,Motivational");
+			System.out.println("4. Sad ");
+			System.out.print("Enter your choice:");
+			int choice=sc.nextInt();
+			if(choice==1)
+			{
+				System.out.println("Welcome to U1   Melody");
+				System.out.println("•Aathadi Manasudhan   ");
+				System.out.println("•Chinna sirisu ");
+				System.out.println("• Venmeggam ");
+				System.out.println("•Iayyayyo");
+			}	
+			else if(choice==2)
+			{
+					
+				System.out.println("Welcome to U1   Kuthu song ");
+				System.out.println("• Machi open the bottle  ");
+				System.out.println("• Saroja saman nikalo  ");	
+				System.out.println("• No money no honey   ");
+				System.out.println("•  Villaiyadu mankatha  ");
+					
+			}
+			else if(choice==3)
+			{
+					
+				System.out.println("Welcome to U1    Motivational Song  song ");
+				System.out.println("•Oru nalil    ");
+				System.out.println("•Nimirnthu NIL    ");	
+				System.out.println("•Edhirthu Nill ");
+				System.out.println("•Ungakkule Mirugam    ");
+					
+			}
+			else if(choice==4)
+			{
+					
+				System.out.println("Welcome to  U1  Kuthu song ");
+				System.out.println("•Kadhal valarthen   ");
+				System.out.println("•Yedho ondru ennai   ");	
+				System.out.println("•Pogadhe   ");
+				System.out.println("•Ninaithu ninaithu    ");
+					
+			}
+			
+				
+			else
+			{
+				System.out.println("invalid song");
+			}
+		}
+		else
+		{
+			System.out.println("invalid play list");
+		}
+		
+		
+
+		
+	}
+}
+
+
+ 
